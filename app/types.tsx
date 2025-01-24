ed4c9c47fd72b5d48805791f35d53fbbd5883313
Azizur Rahman <azizur.rahman59@qmail.cuny.edu>
2025-01-23 19:56:12 -0500

    built the UI

---
+++ app/types.tsx
@@ -0,0 +1,38 @@
+export interface RandomUsers {
+  id: string;
+  name: string;
+  image: string;
+}
+
+export interface PostWithProfile {
+  id: string;
+  user_id: string;
+  video_url: string;
+  text: string;
+  created_at: string;
+  profile: {
+    user_id: string;
+    name: string;
+    image: string;
+  };
+}
+
+////////////////////////////////////
+////////////////////////////////////
+
+// COMPONENT TYPES
+export interface PostMainCompTypes {
+  post: PostWithProfile;
+}
+
+// LAYOUT INCLUDES TYPES
+
+export interface MenuItemsTypes {
+  iconString: string;
+  colorString: string;
+  sizeString: string;
+}
+
+export interface MenuItemFollowCompTypes {
+  user: RandomUsers;
+}
